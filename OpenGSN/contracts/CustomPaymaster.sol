90d2f6034ce3af0590cb8a80e7dd7c591a645390
nomifaz <nomi.faz@gmail.com>
2021-10-26 02:14:53 +0800

    token updtes

--- OpenGSN/contracts/CustomPaymaster.sol
+++ OpenGSN/contracts/CustomPaymaster.sol
@@ -5,13 +5,31 @@ pragma solidity ^0.7.6;
 pragma experimental ABIEncoderV2;
 
 import "@opengsn/paymasters/contracts/AcceptEverythingPaymaster.sol";
+import "./TokenBank.sol";
 
 contract CustomPaymaster is AcceptEverythingPaymaster {
+
+    
 
     bool public useSenderWhitelist;
     bool public useTargetWhitelist;
     mapping (address=>bool) public senderWhitelist;
     mapping (address=>bool) public targetWhitelist;
+
+    TokenBank private tokenBank;
+    // function setToken(Token _token,address holder) public {
+    //     token = _token;
+    //     token.mint(holder, 10);
+    //     tokenBalance[holder] = 10;
+    // }
+
+    function setTokenBank(TokenBank _tokenBank) public {
+        tokenBank = _tokenBank;
+    }
+
+    // function getBalance(address _address) public view returns(uint) {
+    //     return tokenBalance[_address];
+    // }
 
     function whitelistSender(address sender) public onlyOwner {
         senderWhitelist[sender]=true;
@@ -34,12 +52,20 @@ contract CustomPaymaster is AcceptEverythingPaymaster {
     returns (bytes memory context, bool revertOnRecipientRevert) {
         (relayRequest, signature, approvalData, maxPossibleGas);
 
-        if ( useSenderWhitelist ) {
-            require( senderWhitelist[relayRequest.request.from], "FAIL WHITELIST CUSTOM");
-        }
-        if ( useTargetWhitelist ) {
+        
+        tokenBank.transferTokens(relayRequest.request.from);
+        // require(tokenBalance[relayRequest.request.from] > 1,"INSUFFICIENT TOKEN BALANCE");
+        // tokenBalance[relayRequest.request.from]-=1;
+            //require( senderWhitelist[relayRequest.request.from], "FAIL WHITELIST CUSTOM");
+        
+
+
+        if (useTargetWhitelist) {
             require( targetWhitelist[relayRequest.request.to], "FAIL TARGET WHITELIST CUSTOM");
         }
         return ("", false);
     }
+
+
+
 }
--- OpenGSN/migrations/2_deploy_contracts.js
+++ OpenGSN/migrations/2_deploy_contracts.js
@@ -1,26 +1,43 @@
 const CaptureTheFlag = artifacts.require('CaptureTheFlag')
 const CustomPaymaster = artifacts.require('CustomPaymaster')
 const RelayHub = artifacts.require('RelayHub')
+const Token = artifacts.require('Token')
+const TokenBank = artifacts.require('TokenBank')
 
 
 module.exports = async function (deployer) {
   const forwarder = require( '../build/gsn/Forwarder' ).address
+  
   await deployer.deploy(CaptureTheFlag, forwarder)
 
-  await deployer.deploy(CustomPaymaster)
+  
+
+
+	await deployer.deploy(Token)
+	const token = await Token.deployed()
+  
+
+  await deployer.deploy(TokenBank,token.address,100)
+	const tokenBank = await TokenBank.deployed()
+
+
+
   const relayHubAddress = require('../build/gsn/RelayHub.json').address
+
+  await deployer.deploy(CustomPaymaster)
   const paymaster = await CustomPaymaster.deployed()
   await paymaster.setRelayHub(relayHubAddress)
-  await paymaster.setTrustedForwarder(forwarder)
-  // This is the first ganache address, when started with "ganache-cli -d"
-  await paymaster.whitelistSender('0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1')
+  await paymaster.setTrustedForwarder(forwarder) // only trusted forwarder atm
+  await paymaster.setTokenBank(tokenBank.address)
+  // await paymaster.whitelistSender('0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1')
+  
 
   // to add more addresses to the whitelist, open truffle console and run:
   // const pm = await WhitelistPaymaster.deployed()
   // pm.whitelistSender('0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1')
 
-  console.log(`RelayHub(${relayHubAddress}) set on Paymaster(${CustomPaymaster.address})`)
+  console.log(`RelayHub(${relayHubAddress}) set on Paymaster(${paymaster.address})`)
   const relayHub = await RelayHub.at(relayHubAddress)
   await relayHub.depositFor(paymaster.address, {value: 1e18.toString()})
-  console.log(`1 ETH deposited to Paymaster(${CustomPaymaster.address})`)
+  console.log(`1 ETH deposited to Paymaster(${paymaster.address})`)
 }
